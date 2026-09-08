fbac1d018bb61672113d27ab2b83ccf3b4a6dd1e
francisjaneolvido <olvidofrancisjane2003@gmail.com>
2026-09-08 19:24:35 +0800

    feat: create call to action file

---
+++ resources/views/components/cta.blade.php
@@ -0,0 +1,87 @@
+<section
+    class="border-t border-white/5
+           bg-[#0C0C0C] py-24"
+>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+
+        <div
+            class="relative overflow-hidden
+                   rounded-3xl
+                   border border-white/10
+                   bg-[#111111]
+                   px-6 py-16
+                   text-center
+                   sm:px-12
+                   lg:py-20"
+        >
+
+            {{-- SMALL LABEL --}}
+            <p
+                class="text-xs font-bold
+                       uppercase tracking-[0.24em]
+                       text-[#F4510B]"
+            >
+                Ready To Eat?
+            </p>
+
+
+            {{-- HEADING --}}
+            <h2
+                class="mx-auto mt-4
+                       max-w-3xl
+                       text-4xl font-black
+                       uppercase leading-tight
+                       tracking-tight
+                       text-[#F7F3ED]
+                       sm:text-5xl
+                       lg:text-6xl"
+            >
+                The Grill Is Hot.
+                <br>
+
+                Your Meal Is
+                <span class="text-[#F4510B]">
+                    Waiting.
+                </span>
+            </h2>
+
+
+            {{-- DESCRIPTION --}}
+            <p
+                class="mx-auto mt-6
+                       max-w-xl
+                       text-base leading-7
+                       text-zinc-500"
+            >
+                Enjoy smoky Filipino grilled favorites
+                for dine-in or takeout at SmokeHouse
+                in Barangay Central, Quezon City.
+            </p>
+
+
+            {{-- BUTTONS --}}
+            <div
+                class="mt-8 flex
+                       flex-col items-center
+                       justify-center gap-3
+                       sm:flex-row"
+            >
+
+                <x-button href="#pricing">
+                    Get Started
+                </x-button>
+
+
+                <x-button
+                    href="#contact"
+                    variant="secondary"
+                >
+                    Contact Us
+                </x-button>
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
